42d0b68095d8ce7859287f737338c457f89f9977
demesheo <d.wilsonii91@gmail.com>
2015-07-15 15:35:46 -0700

    implement basic functionality for song model/initiate tests

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,20 @@ var SongModel = Backbone.Model.extend({
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
-  }
+  }, 
 
+  enqueue: function(){
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('enqueue', this);
+  }, 
+
+  dequeue: function(){
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('dequeue', this);
+  }, 
+
+  ended: function(){
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('ended', this);
+  }
 });
