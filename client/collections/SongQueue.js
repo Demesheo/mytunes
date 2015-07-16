ee2dc66834092f970c4c92eb7544da05b7cf6bed
demesheo <d.wilsonii91@gmail.com>
2015-07-15 20:01:10 -0700

    passed some more tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,41 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+
+
   initialize: function(){
+
+  },
+
+  playFirst: function(song){
+    // console.log('song',song);
+    this.models[0].play();
+  },
+
+  playNext: function(){
+
+  },
+
+  enqueue: function(song){
+    this.add(song);
+    this.render();
+  },
+
+  dequeue: function(song){
+    this.remove(song);
   }
+
+  // render: function(){
+  //   // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+  //   // see http://api.jquery.com/detach/
+  //   this.$el.children().detach();
+
+  //   this.$el.html('<th>Song Queue</th>').append(
+  //     this.collection.map(function(song){
+  //       return new SongQueueEntryView({model: song}).render();
+  //     })
+  //   );
+  // }
+
 
 });
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -7,6 +7,7 @@ var LibraryEntryView = Backbone.View.extend({
 
   events: {
     'click': function() {
+      // console.log(this.model);
       this.model.enqueue();
     }
   },
